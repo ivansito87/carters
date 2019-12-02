b02f4cf5b2685d6500dfae0d942906a63bf9aaf4
Ivan Rendon <ivansito87@gmail.com>
2019-12-01 22:07:45 -0500

    adds solutions to shop page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import ShopPage from './pages/shoppage/shop.component'
+
 import './App.css';
 
-import HomePage from './pages/homepage/home-page.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
-
-const JacketsPage = () => (
-  <div>
-    <h1>Jackets Page </h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        {/*
-            Prop Tuneling we have access to hystory and match here
-            since is pased from the route compnent
-        */}
-        <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/jackets" component={JacketsPage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import './menu-item.scss';
 
-// In this case the match and history props are part of the props object and are
-// passed ewith every prop any time we need to find this
-const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
-    onClick={() => {
-      history.push(`${match.path}${linkUrl}`);
-    }}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
-      className="background-image"
+      className='background-image'
       style={{
-        backgroundImage: `url(${imageUrl})`,
+        backgroundImage: `url(${imageUrl})`
       }}
     />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
 );
-
-MenuItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired,
-};
 
 export default withRouter(MenuItem);
